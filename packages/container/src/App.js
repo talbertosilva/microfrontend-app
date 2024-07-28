896956dea587c8e95498c018b56a5b403b44293f
talbertosilva <tiagoansilva.9@gmail.com>
2024-07-28 23:04:38 +0100

    Container update

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,5 +2,9 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-    return <MarketingApp />
+    return (
+        <div>
+            <MarketingApp />
+        </div>
+    );
 }
